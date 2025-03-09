ce825faaf70139ed52b0c4dc76a008f65da5883c
MOMO0Devo <mamkm8276@gmail.com>
2025-03-09 16:25:17 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>kamaljan2333333333of moshhhhhhhhhhhthe Tag</div>
+    <div>kamaljan2333333333of moshhhhhhhhhhhthe Tag1</div>
   )
 }
 
